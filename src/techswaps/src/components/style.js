946f04671fe273cfb3961309379f7f95023df3d5
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2023-03-07 16:53:14 -0300

    improving navbar

---
+++ src/techswaps/src/components/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const ContainerNavbar = styled.div`
+    display: flex-end;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
+    width: 355px;
+    height: 56px;
+    
+`;
