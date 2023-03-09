9717f2ad77dd6bd24dd83526e4e9f2eaeccb0c07
matheusmacedosantos <99502202+matheusmacedosantos@users.noreply.github.com>
2023-03-08 23:43:56 -0300

    Finalizacao da homepage

--- src/techswaps/src/components/HeroSection/index.js
+++ src/techswaps/src/components/HeroSection/index.js
@@ -1,14 +1,71 @@
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
-import { HeroBg, HeroContainer } from './HeroElements'
+import { HeroBg, HeroContainer, homeOne, Container, Texto, Titulo, Subtitulo, Imagem, Imagem2, Imagem3, Imagem4, Imagem5, StyledButton, StyledButtonDark } from './HeroElements'
+import { experimentalStyled as styled } from '@mui/material/styles';
+import image from '../HeroSection/img/techswapsblue.png'
+import consultImg from '../HeroSection/img/consultImg.png'
+import meetImg from '../HeroSection/img/meet2.png'
+import blueMeet from '../HeroSection/img/blueMeeting.png'
+import dados from '../HeroSection/img/dados.png'
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 const HeroSection = () => {
     return (
+
         <HeroContainer>
-            <HeroBg/>
+            <HeroBg>
+                <Container>
+                    <Texto>
+                        <Titulo>Bem-vindo(a) ao TechSwaps</Titulo>
+                        <Subtitulo>Participe do desenvolvimento de grandes ideias dentro da Dell.</Subtitulo>
+                    </Texto>
+                    <Imagem src={image} alt="Imagem" />
+                </Container>
+                <Container>
+                    <Imagem2 src={consultImg} alt="Imagem" />
+                    <Texto>
+                        <Titulo>Sobre o TechSwaps</Titulo>
+                        <Subtitulo>A plataforma visa ajudar profissionais a ampliar suas habilidades e conhecimentos, ao mesmo tempo em que ajuda projetos temporários a encontrar profissionais capacitados e interessados em aprender.</Subtitulo>
+                    </Texto>
+                </Container>
+                <Container>
+                    <Texto>
+                        <Titulo>Escolha o que você irá aprender!</Titulo>
+                        <Subtitulo>Podendo ser como shadow ou como tecnico, você poderá escolher a área e o modelo que irá aprender!</Subtitulo>
+                        <StyledButton>Seja Shadow</StyledButton>
+                        <StyledButtonDark>Seja Técnico</StyledButtonDark>
+                    </Texto>
+                    <Imagem3 src={meetImg} alt="Imagem" />
+                </Container>
+                <Container>
+                    <Imagem4 src={blueMeet} alt="Imagem" />
+                </Container>
+                <Container>
+                    <Imagem5 src={dados} alt="Imagem" />
+                    <Texto>
+                        <Titulo>Cadastre projetos em andamento</Titulo>
+                        <Subtitulo>Cadastre um projeto e agregue uma pessoa com a visão de fora ao seu time. Além disso, incentiva a criação de uma rede de contatos entre os profissionais de TI, facilitando futuros aprendizados e projetos. </Subtitulo>
+                        <StyledButton>Proponha um projeto</StyledButton>
+                    </Texto>
+                </Container>
                 
+
+
+
+
+            </HeroBg>
         </HeroContainer>
 
     )
 }
 
 export default HeroSection
+
+
