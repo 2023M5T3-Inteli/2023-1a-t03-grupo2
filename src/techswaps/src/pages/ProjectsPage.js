688f992b35c0f557b79b8bff975040d70303d37a
JuliaTogni <julia.togni@sou.inteli.edu.br>
2023-03-16 17:47:20 -0300

    Changes

--- src/techswaps/src/pages/ProjectsPage.js
+++ src/techswaps/src/pages/ProjectsPage.js
@@ -19,7 +19,7 @@ const ProjectWrapper = styled.div`
   position: relative;
   z-index: 0;
   box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
-    rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+  rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
   margin-left: 10px;
   margin-right: 10px;
   margin-bottom: 10px;
