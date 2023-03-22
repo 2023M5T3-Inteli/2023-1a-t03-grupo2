5d996f8b01626e7f79b65f797a2eca92d831e304
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2023-03-22 10:31:31 -0300

    prisma service code

--- src/backend/src/User/user/user.service.ts
+++ src/backend/src/User/user/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+
+const prisma = new PrismaService()
 
 @Injectable()
 export class UserService {
