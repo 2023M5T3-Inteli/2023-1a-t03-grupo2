ab17a276e7278bb9df05df49d0f722c272772de7
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2023-03-24 11:51:28 -0300

    fixing mistakes

--- src/techswaps/src/components/MastHead.js
+++ src/techswaps/src/components/MastHead.js
@@ -4,6 +4,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Button, Tab, Tabs, Box } from '@mui/material';
 import styled from '@emotion/styled';
+import Popup from "../components/Popup/PopUpCreateProject";
+import FormPage from '../pages/NewProject/Formpage';
 
 const TabContainer = styled(Tabs)`
     width: 100%;
@@ -30,12 +32,15 @@ const MastheadWrapper = styled(Box)`
 
 export default function Masthead() {
   const [selectedTab, setSelectedTab] = React.useState(0);
+  const [buttonPopup, setButtonPopup] = React.useState(false);
+
 
   const handleTabChange = (event, newValue) => {
     setSelectedTab(newValue);
   };
 
   return (
+    <>
     <AppBar position="margin-top: 50px;" sx={{maxWidth: '990px', margin: '0 auto', backgroundColor: 'transparent' }}>
       <MastheadWrapper>
         <Toolbar>
@@ -45,9 +50,11 @@ export default function Masthead() {
             <Tab label="Profile" id="tab-2" />
             <Tab label="Recognition" id="tab-3" />
           </TabContainer>
-          <CustomButton variant="contained">Create project</CustomButton>
+          <CustomButton variant="contained" >Create project</CustomButton>
         </Toolbar>
       </MastheadWrapper>
     </AppBar>
+    
+    </>
   );
 }
--- src/techswaps/src/pages/NewProject/Formpage.js
+++ src/techswaps/src/pages/NewProject/Formpage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import HomeNavbar from "../../components/Navbar";
-//import "./FormPage.css"; //importando o arquivo CSS para personalização
-
+import "./FormPage.css"; //importando o arquivo CSS para personalização
 const FormPage = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -10,8 +9,6 @@ const FormPage = () => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [projeto, setProjeto] = useState([]);
-
-
   useEffect(() => {
     fetch('http://localhost:3001/projetoss')
        .then((response) => response.json())
@@ -23,19 +20,15 @@ const FormPage = () => {
           console.log(err.message);
        });
  }, []);
-
   const handleAddTag = (event) => {
     event.preventDefault();
     const newTag = event.target.elements.tag.value;
     setTags([...tags, newTag]);
     event.target.reset();
   };
-
   const handleSubmit = (event) => {
     event.preventDefault();
-
     // Implementar lógica de envio do formulário aqui
-
     // Resetar os valores do formulário
     setTitle("");
     setDescription("");
@@ -47,93 +40,94 @@ const FormPage = () => {
   console.log(projeto)
   return (
     <>
-    <HomeNavbar></HomeNavbar>
-    
-    <div className="form-container">
-      <h1>Novo Projeto</h1>
+    <h1>Create a new project</h1>
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="title">Título do Projeto:</label>
+        <div className="form-group"></div>
+        <label className="titleForm" htmlFor="title">Title of the project:</label>
           <input
             type="text"
             id="title"
             name="title"
+            className="textSmallInput"
             value={title}
             onChange={(event) => setTitle(event.target.value)}
           />
-        </div>
         <div className="form-group">
           <label htmlFor="description">Descrição:</label>
           <textarea
             id="description"
             name="description"
+            className="textFormInput"
             value={description}
             onChange={(event) => setDescription(event.target.value)}
           />
         </div>
         <div className="form-group">
           <label htmlFor="tags">Tags:</label>
-          <form onSubmit={handleAddTag}>
-            <input type="text" id="tag" name="tag" />
-            <button type="submit" id="tag" > Adicionar Tag</button>
+          <form onSubmit={handleAddTag} className="textSmallInput">
+            <input type="text" id="tag" name="tag" className="textSmallInput" />
+            <button type="submit" id="tag" className="buttonAll"> Adicionar Tag</button>
           </form>
           <ul>
             {tags.map((tag) => (
               <li key={tag}>{tag}</li>
-            ))}
-          </ul>
+              ))}
+            </ul>
+          </div>
+          <div className="form-group">
+            <label htmlFor="coOwner">Co-Owner:</label>
+            <input
+               type="text"
+               id="coOwner"
+               name="coOwner"
+               className="textSmallInput"
+               value={coOwner}
+               onChange={(event) => setCoOwner(event.target.value)}
+             />
+           </div>
+           <div className="textSmallInput">
+          <div className="form-group">
+            <label htmlFor="startDate">Data de Início:</label>
+            <input
+              type="date"
+              id="startDate"
+              name="startDate"
+              value={startDate}
+              onChange={(event) => setStartDate(event.target.value)}
+              />
         </div>
-        <div className="form-group">
-          <label htmlFor="coOwner">Co-Owner:</label>
-          <input
-            type="text"
-            id="coOwner"
-            name="coOwner"
-            value={coOwner}
-            onChange={(event) => setCoOwner(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="startDate">Data de Início:</label>
-          <input
-            type="date"
-            id="startDate"
-            name="startDate"
-            value={startDate}
-            onChange={(event) => setStartDate(event.target.value)}
-          />
 
         </div>
-        <div className="form-group">
-          <label htmlFor="endDate">Data de Término:</label>
-          <input
-            type="date"
-            id="endDate"
-            name="endDate"
-            value={endDate}
-            onChange={(event) => setEndDate(event.target.value)}
-          />
+        <div className="textSmallInput">
+          <div className="form-group">
+            <label htmlFor="endDate">Data de Término:</label>
+            <input
+              type="date"
+              id="endDate"
+              name="endDate"
+              value={endDate}
+              onChange={(event) => setEndDate(event.target.value)}
+              />
+          </div>
         </div>
         <button
           type="submit"
-          className="btn-submit"
+          className="buttonAll"
         >
           Adicionar Projeto
         </button>
       </form>
-    </div>
-    {projeto.map((project) => {
-         return (
-          <>
-          <div key={project.id}>
-            <h2>{project.name}</h2>
-            <p >{project.description}</p>
-          </div>
-          </>
-         );
-      })}
-    </>
-  );
+      {projeto.map((project) => {
+        return (
+         <>
+         <div key={project.id}>
+           <h2>{project.name}</h2>
+           <p >{project.description}</p>
+         </div>
+         </>
+        );
+     })}
+   </>
+ );
 };
-
 export default FormPage;
