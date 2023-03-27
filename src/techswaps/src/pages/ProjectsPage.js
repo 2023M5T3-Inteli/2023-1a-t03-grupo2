b5f2fe317a3be2f1d7dbaf8a72e54f62802e24e8
matheusmacedosantos <99502202+matheusmacedosantos@users.noreply.github.com>
2023-03-27 13:39:50 -0300

    Comentei button

--- src/techswaps/src/pages/ProjectsPage.js
+++ src/techswaps/src/pages/ProjectsPage.js
@@ -164,7 +164,7 @@ const baseUrl = "http://localhost:3001/";
 
 export const ProjectsPage = () => {
   const [projetos, setProjeto] = useState(null);
-  const [buttonPopup, setButtonPopup] = useState(false);
+  //const [buttonPopup, setButtonPopup] = useState(false);
 
 
   const projects = [
@@ -200,59 +200,59 @@ export const ProjectsPage = () => {
   return (
     <>
       <HomeNavbar></HomeNavbar>
-        <PageContainer>
-          <MastHead />
-          <TitleWrapper>
-            <div>Explore os projetos disponíveis</div>
-          </TitleWrapper>
-          <SearchInput>
-            <input placeholder="Pesquise projetos"></input>
-            <SearchIcon />
-            <select placeholder="Filtrar por">
-              <option value="opcao0"></option>
-              <option value="opcao1">React</option>
-              <option value="opcao2">Java</option>
-              <option value="opcao3">UX Design</option>
-            </select>
-          </SearchInput>
-          <StyledButton onClick={() => setButtonPopup(true)}>Add project</StyledButton>
-          {projects.map((item) => {
-            return (
-              <>
-                <ProjectWrapper>
-                  <ProjectCard
-                    title={<Title>{item.title}</Title>}
-                    stats={
-                      <Stats
-                        style={{
-                          borderColor:
-                            item.stats === "In progress"
-                              ? "orange"
-                              : item.stats === "Recruiting"
+      <PageContainer>
+        <MastHead />
+        <TitleWrapper>
+          <div>Explore os projetos disponíveis</div>
+        </TitleWrapper>
+        <SearchInput>
+          <input placeholder="Pesquise projetos"></input>
+          <SearchIcon />
+          <select placeholder="Filtrar por">
+            <option value="opcao0"></option>
+            <option value="opcao1">React</option>
+            <option value="opcao2">Java</option>
+            <option value="opcao3">UX Design</option>
+          </select>
+        </SearchInput>
+        <StyledButton onClick={() => setButtonPopup(true)}>Add project</StyledButton>
+        {projects.map((item) => {
+          return (
+            <>
+              <ProjectWrapper>
+                <ProjectCard
+                  title={<Title>{item.title}</Title>}
+                  stats={
+                    <Stats
+                      style={{
+                        borderColor:
+                          item.stats === "In progress"
+                            ? "orange"
+                            : item.stats === "Recruiting"
                               ? "green"
                               : "red",
-                          color:
-                            item.stats === "In progress"
-                              ? "orange"
-                              : item.stats === "Recruiting"
+                        color:
+                          item.stats === "In progress"
+                            ? "orange"
+                            : item.stats === "Recruiting"
                               ? "green"
                               : "red",
-                        }}
-                      >
-                        {item.stats}
-                      </Stats>
-                    }
-                    description={<Description>{item.description}</Description>}
-                    tag={<Tag>{item.tag}</Tag>}
-                  />
-                </ProjectWrapper>
-              </>
+                      }}
+                    >
+                      {item.stats}
+                    </Stats>
+                  }
+                  description={<Description>{item.description}</Description>}
+                  tag={<Tag>{item.tag}</Tag>}
+                />
+              </ProjectWrapper>
+            </>
           );
         })}
       </PageContainer>
-    <Popup trigger={buttonPopup}>
+      <Popup trigger={buttonPopup}>
         <FormPage></FormPage>
-    </Popup>
+      </Popup>
     </>
   );
 };
