bc85182882c9ac81e990611dd7df86fe61f91edc
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2023-03-31 11:13:17 -0300

    new resource

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -7,9 +7,12 @@ import { ProjetoController } from './Projeto/projeto/projeto.controller';
 import { SolicitacaoController } from './Solicitacao/solicitacao/solicitacao.controller';
 import { UserController } from './User/user/user.controller';
 import { VagasController } from './Vagas/vagas/vagas.controller';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsuariosModule],
   controllers: [AppController, UserController, SolicitacaoController, VagasController, ProjetoController],
   providers: [AppService, PrismaService],
 })
